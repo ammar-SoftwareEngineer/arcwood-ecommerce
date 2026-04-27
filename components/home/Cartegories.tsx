213abfd3a0f442df330a21b2464db3d9aaf93f71
Ammar Yasser Tolba <ammartolba89@192.168.1.7>
2026-04-27 08:30:03 +0300

    update

--- components/home/Cartegories.tsx
+++ components/home/Cartegories.tsx
@@ -4,8 +4,8 @@ export default function Cartegories() {
 
   return (
     <section className="categories-section py-12 ">
-      <div className="container mx-auto px-20 py-12">
-        <div className="header-section mb-10">
+      <div className="container mx-auto lg:px-20 py-12">
+        <div className="header-section mb-10 px-6 lg:px-0">
           <h2 className="text-4xl font-bold text-main">Categories</h2>
           <p className="mt-3 text-gray-500">
             Browse our main product categories
