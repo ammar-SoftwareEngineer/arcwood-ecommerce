8e94c0d91f432ab2b55f3c049c3e4418cdc6abe6
ammar-SoftwareEngineer <ammartolba89@gmail.com>
2026-05-19 17:19:20 +0300

    update

--- src/components/about/WhyUs.tsx
+++ src/components/about/WhyUs.tsx
@@ -8,7 +8,7 @@ import { whyFeatureIcons } from "../why/whyFeatureIcons";
 
 
 
-export default function WhyUs( why:Promise<WhyUs | null> ) {
+export default function WhyUs({ why }: { why: WhyUs }) {
   const locale = useLocale();
   const isAr = locale === "ar";
   const subtitle = isAr ? why.subtitleAr : why.subtitle;
