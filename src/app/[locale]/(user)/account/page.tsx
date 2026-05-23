540ab4db33c7c3d9cb9f2712832428f1be1fc50e
Ammar Yasser Tolba <ammartolba89@192.168.1.6>
2026-05-24 02:06:08 +0300

    update

--- src/app/[locale]/(user)/account/page.tsx
+++ src/app/[locale]/(user)/account/page.tsx
@@ -1,3 +1,11 @@
+import HeroPages from "@/components/layout/hero/HeroPages";
+
 export default function AccountPage() {
-  return <main className="p-4">Protected account page</main>;
+
+  return <section>
+    <HeroPages />
+    <div className="container mx-auto px-4 py-12 sm:px-6 lg:px-8">
+      <h1>Account</h1>
+    </div>
+  </section>;
 }
--- src/app/[locale]/(user)/orders/page.tsx
+++ src/app/[locale]/(user)/orders/page.tsx
@@ -1,7 +1,12 @@
+import HeroPages from "@/components/layout/hero/HeroPages";
+
 export default function OrdersPage() {
     return (
-        <div>
-            <h1>Orders</h1>
-        </div>
+        <section>
+            <HeroPages />
+            <div className="container mx-auto px-4 py-12 sm:px-6 lg:px-8">
+                <h1>Orders</h1>
+            </div>
+        </section>
     )
 }
